c8ffc969d8465e1d8959c1d92d1fd0234717c3ce
devwithsmile <Devendra.Rameshsaini@cognizant.com>
2023-04-30 13:09:54 +0530

    number guessing  assignment

--- Week-4/session-9/game_object/script.js
+++ Week-4/session-9/game_object/script.js
@@ -32,10 +32,10 @@ game.points = game.coins * 10;
 console.log("points = " + game.points);  // should be 20
 game.playerDies();
 console.log("lives = " + game.lives);    // should be 2
-game.playerDies();
-game.playerDies();
-game.playerDies();
-console.log("lives = " + game.lives);    // should be 0
+game.playerDies(); //1
+game.playerDies(); //0
+game.playerDies(); // 3
+console.log("lives = " + game.lives);    // should be 0 
 game.newGame();
 console.log("lives = " + game.lives);    // should be 3
 console.log("coins = " + game.coins);    // should be 0
